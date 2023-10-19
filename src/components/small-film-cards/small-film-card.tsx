0c4e42e71a6a243bdb6b80ec8f34c38edc6a9dd1
PsevdoKot <youkos00@mail.ru>
2023-10-19 16:38:54 +0500

    Add new components and states

--- src/components/small-film-cards/small-film-card.tsx
+++ src/components/small-film-cards/small-film-card.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Link, generatePath } from 'react-router-dom';
 import { ROUTES } from '../../routes';
+import Screen404 from '../../pages/404-screen/404-screen';
 
 export type SmallFilmCardProps = {
   id: number;
@@ -13,9 +14,13 @@ export type SmallFilmCardProps = {
 }
 
 export function SmallFilmCard({ id, imageSource, name, trailerSource, hoveredCardId: focusedCardId, handleMouseEnter, handleMouseLeave }: SmallFilmCardProps): JSX.Element {
+  if (!focusedCardId) {
+    return <Screen404 />;
+  }
   return (
     <article className="small-film-card catalog__films-card" onMouseEnter={() => handleMouseEnter(id)} onMouseLeave={handleMouseLeave}>
       <div className="small-film-card__image">
+        <video src={trailerSource}></video>
         <img src={imageSource} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
