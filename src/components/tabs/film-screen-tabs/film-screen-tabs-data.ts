9c12e11fa49efa130c4b1784b5a9d4f54f4f2564
PsevdoKot <youkos00@mail.ru>
2023-10-23 14:33:51 +0500

    Implement tabs in film screen

--- src/components/tabs/film-screen-tabs/film-screen-tabs-data.ts
+++ src/components/tabs/film-screen-tabs/film-screen-tabs-data.ts
@@ -1,10 +1,10 @@
 import FilmDetails from './film-details';
 import FilmOverview from './film-overview';
 import FilmReviews from './film-reviews/film-reviews';
-import { TabData } from './tab-data-type';
 import { FilmDetailsProps } from './film-details';
 import { FilmOverviewProps } from './film-overview';
 import { FilmReviewsProps } from './film-reviews/film-reviews';
+import { TabData } from '../tab-data-type';
 
 export type FilmScreenTabsType = FilmOverviewProps & FilmDetailsProps & FilmReviewsProps;
 
