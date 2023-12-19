d58571c5b187ef5a60f97d3652f67e239c59eb34
PsevdoKot <youkos00@mail.ru>
2023-12-19 15:05:58 +0500

    That is the end

--- src/components/genres-filter/genres-filter.test.tsx
+++ src/components/genres-filter/genres-filter.test.tsx
@@ -7,23 +7,27 @@ import userEvent from '@testing-library/user-event';
 import { extractActionsTypes } from '../../utils/mocks';
 import { setGenre } from '../../store/genre-process/genre-process';
 import { ALL_GENRES } from '../../consts';
+import { mockFilmsShortData } from '../../mocks/films-short-data';
 
 describe('Component: GenreFilter', () => {
   it('renders correctly', () => {
     const { withStoreComponent } = withStore(<GenresFilter />, {
-      Genre: { genre: ALL_GENRES }
+      FilmsData: { filmsData: mockFilmsShortData },
+      Genre: { genre: ALL_GENRES },
     });
+    const expectedLength = [...new Set(mockFilmsShortData.map((filmData) => filmData.genre))].length + 1;
 
     render(withStoreComponent);
     const activeButtons = screen.getAllByTestId('genre-button').filter((btn) => btn.classList.contains('catalog__genres-item--active'));
 
-    expect(screen.getAllByTestId('genre-button')).toHaveLength(Object.values(genres).length);
+    expect(screen.getAllByTestId('genre-button')).toHaveLength(expectedLength);
     expect(activeButtons).toHaveLength(1);
   });
 
   it('dispatch "setGenre" when user click genre button', async () => {
     const expectedGenre = genres.Drama;
     const { withStoreComponent, mockStore } = withStore(<GenresFilter />, {
+      FilmsData: { filmsData: mockFilmsShortData },
       Genre: { genre: genres.Comedy }
     });
 
